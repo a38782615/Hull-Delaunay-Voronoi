86c0201d32fd6d8aad58782e2759fb1f51eaafe6
a38782615 <38782615@qq.com>
2024-12-04 17:28:41 +0800

    优化一下

--- Assets/Res/MyMap/DrawMap.cs
+++ Assets/Res/MyMap/DrawMap.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
-using Random = Unity.Mathematics.Random;
 
 namespace MyMap
 {
     public struct UVTile
     {
+        public static int TileCount = 7;
         public static int tileSize = 136;
-        public static float atlasWidth = 1024f;
+        public static int atlasWidth = 1024;
+        public static float cellSize = tileSize*1f / atlasWidth;
         public int Id;
         public int2 position;
         public Rect uvRect;
 
-        public UVTile(int id, int2 pos)
+        public UVTile(int2 pos)
         {
-            Id = id;
+            Id = GetId(pos.x, pos.y);
             position = pos;
-            var tx = pos.x * tileSize;
-            var ty = atlasWidth - (pos.y + 1) * tileSize;
-            uvRect = new Rect(tx / atlasWidth, ty / atlasWidth, tileSize / atlasWidth, tileSize / atlasWidth);
+            var tx = position.x * cellSize;
+            var ty = 1 - (position.y + 1) * cellSize;
+            uvRect = new Rect(tx , ty , cellSize, cellSize);
         }
 
+        public static int GetId(int x, int y)
+        {
+            x %= TileCount;
+            y %= TileCount;
+            return x + y * TileCount + 1;
+        }
         public override int GetHashCode()
         {
             return position.x * 10000 + position.y;
@@ -32,16 +38,18 @@ namespace MyMap
     public struct UVTile2
     {
         public static int tileSize = 64;
-        public static float atlasWidth = 512f;
+        public static int atlasWidth = 512;
+        public static int count = atlasWidth / tileSize;
+        public static float cellSize = tileSize*1f / atlasWidth;
         public int2 position;
         public Rect uvRect;
 
         public UVTile2(int2 pos)
         {
-            position = pos % 7;
-            var tx = pos.x * tileSize;
-            var ty = pos.y * tileSize;
-            uvRect = new Rect(tx / atlasWidth, ty / atlasWidth, tileSize / atlasWidth, tileSize / atlasWidth);
+            position = pos % count;
+            var tx = position.x * cellSize;
+            var ty = position.y * cellSize;
+            uvRect = new Rect(tx , ty , cellSize, cellSize);
         }
 
         public override int GetHashCode()
@@ -54,7 +62,6 @@ namespace MyMap
     [RequireComponent(typeof(MeshRenderer))]
     public class DrawMap : MonoBehaviour
     {
-        public Material material;
         public MeshRenderer m_meshRenderer;
         public MeshFilter meshFilter;
         public Texture2D mainTexture;
@@ -72,55 +79,30 @@ namespace MyMap
         private List<int> s_triangles;
         public Dictionary<int, UVTile> m_uvMap;
 
-        public Dictionary<int, int> Masks = new Dictionary<int, int>()
-        {
-            { 2, 1 }, { 8, 2 }, { 10, 3 }, { 11, 7 }, { 16, 5 },
-            { 18, 6 }, { 22, 4 }, { 24, 8 }, { 26, 9 }, { 27, 10 },
-            { 30, 11 }, { 31, 3 }, { 64, 13 }, { 66, 14 }, { 72, 15 },
-            { 74, 16 }, { 75, 17 }, { 80, 18 }, { 82, 19 }, { 86, 20 },
-            { 88, 21 }, { 90, 22 }, { 91, 23 }, { 94, 24 }, { 95, 25 },
-            { 104, 13 }, { 106, 27 }, { 107, 5 }, { 120, 29 }, { 122, 30 },
-            { 123, 31 }, { 126, 32 }, { 127, 33 }, { 208, 10 }, { 210, 35 },
-            { 214, 2 }, { 216, 37 }, { 218, 38 }, { 219, 39 }, { 222, 40 },
-            { 223, 41 }, { 248, 9 }, { 250, 43 }, { 251, 44 }, { 254, 45 },
-            { 255, 49 }, { 0, 46 }
-        };
-
         private void CreateMesh()
         {
             m_uvMap = new Dictionary<int, UVTile>();
+            s_vertices = new List<Vector3>();
+            s_triangles = new List<int>();
+            m_uv = new List<Vector2>();
+            m_uv2 = new List<Vector2>();
             meshFilter = GetComponent<MeshFilter>();
-            m_meshRenderer = GetComponent<MeshRenderer>();
-            if (meshFilter.sharedMesh == null)
+            meshFilter.sharedMesh = new Mesh
             {
-                meshFilter.sharedMesh = new Mesh();
-                meshFilter.sharedMesh.hideFlags = HideFlags.HideAndDontSave;
-                meshFilter.sharedMesh.name = name + "_mesh";
-            }
-
-            m_meshRenderer.material = material;
+                hideFlags = HideFlags.HideAndDontSave,
+                name = name + "_mesh"
+            };
+            meshFilter.sharedMesh.Clear();
+            
+            m_meshRenderer = GetComponent<MeshRenderer>();
             if (m_matPropBlock == null)
             {
                 m_matPropBlock = new MaterialPropertyBlock();
             }
-
-            m_meshRenderer.GetPropertyBlock(m_matPropBlock);
-            if (mainTexture != null)
-            {
-                m_matPropBlock.SetTexture("_MainTex", mainTexture);
-                m_matPropBlock.SetTexture("_Texture2DCover", overlayTexture);
-            }
-
+            m_matPropBlock.SetTexture("_MainTex", mainTexture);
+            m_matPropBlock.SetTexture("_Texture2DCover", overlayTexture);
             m_meshRenderer.SetPropertyBlock(m_matPropBlock);
-
-            if (s_vertices == null)
-            {
-                s_vertices = new List<Vector3>();
-                s_triangles = new List<int>();
-                m_uv = new List<Vector2>();
-                m_uv2 = new List<Vector2>();
-            }
-
+            
             s_vertices.Clear();
             s_triangles.Clear();
             m_uv.Clear();
@@ -130,14 +112,13 @@ namespace MyMap
         //在维诺图上画地图的图块
 
         Vector2[] s_tileUV = new Vector2[4];
-
-        void DrawOne(Vector2Int pos, Rect tileUV0, Rect tileUV1)
+        void DrawOne(Rect posRect, Rect tileUV0, Rect tileUV1)
         {
             //顶点位置
-            float px0 = pos.x * cellSize.x;
-            float py0 = pos.y * cellSize.y;
-            float px1 = pos.x * cellSize.x + cellSize.x;
-            float py1 = pos.y * cellSize.y + cellSize.y;
+            float px0 = posRect.xMin;
+            float py0 = posRect.yMin;
+            float px1 = posRect.xMax;
+            float py1 = posRect.yMax;
 
             int vertexIdx = s_vertices.Count;
             s_vertices.Add(new Vector3(px0, py0, 0));
@@ -151,7 +132,6 @@ namespace MyMap
             s_triangles.Add(vertexIdx + 0);
             s_triangles.Add(vertexIdx + 3);
             s_triangles.Add(vertexIdx + 1);
-
             //UV贴图坐标
             float u00 = tileUV0.xMin;
             float v00 = tileUV0.yMin;
@@ -181,66 +161,102 @@ namespace MyMap
             }
         }
 
-        public int TileCount = 7;
-        public int cellW = 136;
-        private Vector2 cellSize;
         public int width = 10;
         public int height = 10;
 
-        public List<UVTile> tiles = new List<UVTile>();
-
-        public int GetId(int x, int y)
-        {
-            x = x % TileCount;
-            y = y % TileCount;
-            return x + y * TileCount + 1;
-        }
-
+        public static int[] maski = new int[] { 0, 1 << 3, 0, 1 << 1, 1 << 4, 1 << 2, 0, 1, 0 };
+        public static Dictionary<int, int> Masks;
         void Draw()
         {
+            //0 1 0
+            //2 16 4
+            //0 8 0
+            maski = new int[] { 0, 1 << 3, 0, 1 << 1, 1 << 4, 1 << 2, 0, 1, 0 };
+            Masks = new Dictionary<int, int>(){
+                { 0, 1 }, { 29, 2 }, { 30, 3 }, { 28, 4 }, { 27, 5 },{ 18, 6 }, { 26, 7 }, 
+                { 24, 8 }, { 23, 9 }, { 21, 10 },{ 33, 11 }, { 32, 12 }, { 19, 13 }, { 66, 14 }, 
+                { 72, 15 },{ 16, 16 }, { 31, 17 }, { 80, 18 }, { 82, 19 }, { 86, 20 },{ 88, 21 }, 
+                { 90, 22 }, { 91, 23 }, { 94, 24 }, { 95, 25 }, { 104, 26 }, { 106, 27 }, { 107, 28 },
+                { 120, 29 }, { 122, 30 },{ 123, 31 }, { 126, 32 }, { 127, 33 }, { 208, 34 }, { 210, 35 },
+                { 214, 36 }, { 216, 37 }, { 218, 38 }, { 219, 39 }, { 222, 40 }, {223, 41 }, { 248, 42 }, 
+                { 250, 43 }, { 251, 44 }, { 254, 45 },{ 255, 45 }, { 15, 46 }
+            }; 
+            
             m_uvMap.Clear();
-            for (int j = 0; j < TileCount; j++)
+            for (int j = 0; j < UVTile.TileCount; j++)
             {
-                for (int i = 0; i < TileCount; i++)
+                for (int i = 0; i < UVTile.TileCount; i++)
                 {
-                    var uv = new UVTile(GetId(i, j), new int2(i, j));
+                    var uv = new UVTile(new int2(i, j));
                     m_uvMap.Add(uv.Id, uv);
                 }
             }
-
-            cellSize = new Vector2(cellW / UVTile.atlasWidth, cellW / UVTile.atlasWidth);
 
             for (int j = 0; j < height; j++)
             {
                 for (int i = 0; i < width; i++)
                 {
-                    var mask = GetMaskFromMap(i, j);
-                    Debug.Log($"{i} : {j} : mask:" + mask);
+                    var hasNode = HasNode(i, j);
+                    var uvtile2 = new UVTile2(new int2(i, j));
+                    var mask = GetMaskFromMap(i, j, true);
                     var id = Masks[mask];
-                    var uvtile = new UVTile2(new int2(i, j));
-                    DrawOne(new Vector2Int(i, j), uvtile.uvRect, m_uvMap[id].uvRect);
+                    DrawOne(new Rect(i * UVTile.cellSize, j * UVTile.cellSize, UVTile.cellSize, UVTile.cellSize), 
+                        uvtile2.uvRect,
+                        m_uvMap[id].uvRect);
+                    // if (!hasNode)
+                    // {
+                    //     var mask2 = GetMaskFromMap(i, j, false);
+                    //     var id2 = SubMasks[mask];
+                    // }
                 }
             }
         }
 
-        int[] maski = new int[] { 1, 2, 4, 8, 0, 16, 32, 64, 128 };
+        private bool HasNode(int x, int y)
+        {
+            var ret = false;
+            if((x > -1 && x < width) && (y > -1 && y < height))
+            {
+                ret = true;
+                if (x == width / 2 && y == height / 2)
+                {
+                    ret = false;
+                }
+            }
 
-        private int GetMaskFromMap(int x, int y)
+            return ret;
+        }
+        
+        private int GetMaskFromMap(int x, int y,bool hasNode)
         {
             var mask = 0;
             for (int j = -1; j < 2; j++)
             {
                 for (int i = -1; i < 2; i++)
                 {
+                    var xx = x + i;
+                    var yy = y + j;
+                    
                     var xi = i + 1;
                     var yi = j + 1;
                     var id = xi + yi * 3;
                     var msk = maski[id];
-                    var xx = x + i;
-                    var yy = y + j;
-                    mask += msk * (xx < 0 || yy < 0 || xx >= width || yy >= height ? 0 : 1);
+                    if (hasNode)
+                    {
+                        msk = (HasNode(xx,yy) ? msk: 0);
+                    }
+                    else
+                    {
+                        msk = (!HasNode(xx,yy) ? msk: 0);
+                    }
+                    mask +=  msk;
+                    if (msk > 0)
+                    {
+                        Debug.Log($"msk1 {i} : {j} :" + msk);
+                    }
                 }
             }
+            Debug.Log($"{x} : {y} : mask1:" + mask);
 
             return mask;
         }
